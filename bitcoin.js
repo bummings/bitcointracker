8f114b2bb4f3114ff158dfc47b17e91e59493a0f
fauxkno <sup@fauxkno.com>
2018-02-05 15:38:14 -0500

    fixing initial commit

---
+++ bitcoin.js
@@ -0,0 +1,29 @@
+var btn = document.querySelector('#btn');
+var box = document.getElementById('resultBox');
+
+btn.addEventListener('click', function() {
+	loadPrice();
+});
+
+	function loadPrice() {
+		var XHR = new XMLHttpRequest();
+	XHR.open('GET', 'https://api.coindesk.com/v1/bpi/currentprice.json');
+	XHR.onreadystatechange = function() {
+		if(XHR.readyState == 4 && XHR.status == 200) {
+			
+			var resultUSD = JSON.parse(XHR.responseText).bpi.USD.rate_float;
+			var resultEUR = JSON.parse(XHR.responseText).bpi.EUR.rate_float;
+			var resultGBP = JSON.parse(XHR.responseText).bpi.GBP.rate_float;
+
+			console.log('USD: ' + resultUSD);
+			console.log('EUR: ' + resultEUR);
+			console.log('GBP: ' + resultGBP);
+			
+			box.innerHTML = '<h2> USD: ' + resultUSD +   ' <br>EUR: ' + resultEUR + '<br> GBP: ' + resultGBP + '</h2>';
+			// box.innerHTML= '<h2> EUR: ' + resultEUR + '</h2>';
+		}
+	}
+	XHR.send();
+};
+	
+
